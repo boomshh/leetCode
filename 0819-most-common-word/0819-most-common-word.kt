d0f0ac06858f4931bb3ff34ad7f8f828a75441b5
Ethan <133577209+boomshh@users.noreply.github.com>
2023-12-02 17:10:03 +0900

    Time: 224 ms (76.92%), Space: 40.1 MB (71.79%) - LeetHub

---
+++ 0819-most-common-word/0819-most-common-word.kt
@@ -0,0 +1,13 @@
+class Solution {
+    fun mostCommonWord(paragraph: String, banned: Array<String>): String {
+        val counts: MutableMap<String, Int> = mutableMapOf()
+        val words = paragraph.replace("\\W+".toRegex(), " ").toLowerCase().trim().split(" ")
+        
+        for (w in words) {
+            if(!banned.contains(w)) {
+                counts[w] = counts.getOrDefault(w, 0) + 1
+            }
+        }
+        return counts.maxBy { it.value }!!.key
+    }
+}
